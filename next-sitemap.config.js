acfadc26228befad4f45e6bfdd1b2a54b011d260
Ayush <tuleayush2@gmail.com>
2024-09-09 14:53:45 +0530

    SEO related changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.zenqua.com',
+  generateRobotsTxt: true, // (optional) Generates a robots.txt file
+  // Additional options
+};
