a955ea6088fc62252268e908ba36e5c48807022e
tuleAyush <tuleayush2@gmail.com>
2025-01-07 17:35:20 +0530

    update metatags

--- app/(auth)/(company)/career/layout.tsx
+++ app/(auth)/(company)/career/layout.tsx
@@ -7,9 +7,8 @@ export async function generateMetadata(): Promise<Metadata> {
 
   return {
     metadataBase,
-    title: "Career Openings | ZenQua Technologies Pvt Ltd",
-    description:
-      "Explore exciting career opportunities at ZenQua Technologies. Work with industry experts on cutting-edge IT projects. Apply now for roles like DevOps Engineer, Java Developer, and more.",
+    title:"Career Opportunities In software Development | Explore Open Positions",
+    description:"Join Our Team | Explore Open Positions at Our Software Development Company | Zenqua Technologies",
     keywords:
       "Job Openings, ZenQua Jobs, ReactJS Jobs, Frontend Developer Careers, Backend Developer Jobs, Full Stack Developer Roles, ASP.Net Developer Jobs, Power BI Developer Careers, DevOps Engineer Openings, Java Developer Positions, Power Apps Developer Opportunities, IT Jobs Near Me, Software Developer Careers, Digital Marketing Jobs, Mobile App Developer Jobs, UI/UX Designer Roles, Data Analyst Jobs, Cloud Engineer Openings, IT Project Manager Careers, Walk-In IT Jobs, Tech Careers at ZenQua, Remote IT Jobs, Emerging Technology Careers, Innovation in IT Careers, Future-Ready IT Jobs, Technology Jobs in Indore, Career in Web Development, ZenQua Technologies Careers, Software Engineer Openings, IT Internships, Experienced IT Jobs, Growth-Oriented Careers, Collaborative Work Environment Jobs, Trending IT Jobs, Career in Digital Innovation",
     icons: {
